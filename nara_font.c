41d9014fe7b853e5f3e9a6554dd51db04a65fba3
Ren <73724137+pra1rie@users.noreply.github.com>
2025-10-26 21:04:22 -0300

    shrink code

--- nara_font.c
+++ nara_font.c
@@ -1,5 +1,4 @@
 #include "nara_font.h"
-#include "nara_window.h"
 
 #include <string.h>
 
@@ -22,39 +21,30 @@ fontFree(Font *font)
 	TTF_CloseFont(font->font);
 }
 
+#define __NARA_FONT_WRITE(X, Y, W, H) \
+	if (strlen(text) == 0) return; \
+	SDL_Surface *srf = TTF_RenderUTF8_Blended(font->font, text, font->color); \
+	SDL_Texture *tex = SDL_CreateTextureFromSurface(dest, srf); \
+	SDL_RenderCopy(dest, tex, NULL, &((SDL_Rect) {X, Y, W, H})); \
+	SDL_DestroyTexture(tex); \
+	SDL_FreeSurface(srf); \
+
 void
 fontWrite(Font *font, SDL_Renderer *dest, char *text, int x, int y)
 {
-	if (strlen(text) == 0) return;
-	SDL_Surface *srf = TTF_RenderUTF8_Blended(font->font, text, font->color);
-	SDL_Texture *tex = SDL_CreateTextureFromSurface(dest, srf);
-	SDL_Rect rect = {x, y, srf->w, srf->h};
-	SDL_RenderCopy(dest, tex, NULL, &rect);
-	SDL_DestroyTexture(tex);
-	SDL_FreeSurface(srf);
+	__NARA_FONT_WRITE(x, y, srf->w, srf->h);
 }
 
 void
 fontWriteCenter(Font *font, SDL_Renderer *dest, char *text, int x, int y)
 {
-	if (strlen(text) == 0) return;
-	SDL_Surface *srf = TTF_RenderUTF8_Blended(font->font, text, font->color);
-	SDL_Texture *tex = SDL_CreateTextureFromSurface(dest, srf);
-	SDL_Rect rect = {x - (srf->w/2), y - (srf->h/2), srf->w, srf->h};
-	SDL_RenderCopy(dest, tex, NULL, &rect);
-	SDL_DestroyTexture(tex);
-	SDL_FreeSurface(srf);
+	__NARA_FONT_WRITE(x - (srf->w/2), y - (srf->h/2), srf->w, srf->h);
 }
 
 void
 fontWriteRight(Font *font, SDL_Renderer *dest, char *text, int x, int y)
 {
-	if (strlen(text) == 0) return;
-	SDL_Surface *srf = TTF_RenderUTF8_Blended(font->font, text, font->color);
-	SDL_Texture *tex = SDL_CreateTextureFromSurface(dest, srf);
-	SDL_Rect rect = {x - srf->w, y, srf->w, srf->h};
-	SDL_RenderCopy(dest, tex, NULL, &rect);
-	SDL_DestroyTexture(tex);
-	SDL_FreeSurface(srf);
+	__NARA_FONT_WRITE(x - srf->w, y, srf->w, srf->h);
 }
 
+#undef __NARA_FONT_WRITE
